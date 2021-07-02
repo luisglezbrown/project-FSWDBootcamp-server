9614d801bbbcaf2c465299e79b0afafd6a693fa2
luisglezbrown <lfernanglez@gmail.com>
2021-07-02 17:55:51 +0200

    Creo la entidad City

---
+++ src/Entity/City.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CityRepository::class)
+ */
+class City
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $imgpath;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $ranking;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImgpath(): ?string
+    {
+        return $this->imgpath;
+    }
+
+    public function setImgpath(string $imgpath): self
+    {
+        $this->imgpath = $imgpath;
+
+        return $this;
+    }
+
+    public function getRanking(): ?int
+    {
+        return $this->ranking;
+    }
+
+    public function setRanking(int $ranking): self
+    {
+        $this->ranking = $ranking;
+
+        return $this;
+    }
+}
