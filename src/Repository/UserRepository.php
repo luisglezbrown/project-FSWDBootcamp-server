3e6762eb2895d6b589788743c7ebbf377d29dc0a
luisglezbrown <lfernanglez@gmail.com>
2021-07-07 17:18:09 +0200

    Se genera el método upgradePassword al hacer make:user

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,6 +5,9 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
@@ -12,11 +15,25 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
+        }
+
+        $user->setPassword($newHashedPassword);
+        $this->_em->persist($user);
+        $this->_em->flush();
     }
 
     /**
@@ -26,8 +43,10 @@ class UserRepository extends ServiceEntityRepository
     public function latestGuides()
     {
         return $this->createQueryBuilder('c')
-            ->setParameter('val', "guide")
-            ->andWhere('c.role = :val')
+           ->andWhere('c.roles LIKE :role')
+           ->setParameter('role', '%ROLE_GUIDE%')
+            // ->andWhere('c.roles = :val')
+            // ->setParameter('val', "ROLE_GUIDE")
             ->orderBy('c.since', 'DESC')
             ->setMaxResults(4)
             ->getQuery()
